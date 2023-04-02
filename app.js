1c867ccdf3a5d580435fd29800f8a88d7553a181
wanyuew <wang.wanyu@northeastern.edu>
2023-04-02 01:50:48 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import HelloController from "./controllers/hello-controller.js";
+import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
+
+const app = express();
+app.use(express.json());
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+TuitsController(app);
+HelloController(app);
+UserController(app);
+app.listen(4000);
