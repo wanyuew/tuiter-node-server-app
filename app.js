501a17ed417408c408580060e7f8843a52c7d016
wanyuew <wang.wanyu@northeastern.edu>
2023-04-02 17:50:39 -0700

    done

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from 'express';
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 // app.get('/hello', (req, res) => {res.send('Life is good!')})
 // app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
